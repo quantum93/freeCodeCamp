945fbbdf31f8bb0570722cb424c8add66ca92863
Tae Lee <tae@Taeui-iMac.local>
2020-06-15 12:34:24 -0700

    first commit for test

---
+++ basicAlgorithmScript.js
@@ -0,0 +1,19 @@
+function largestOfFour(arr) {
+    let maxNumber = 0;
+    let maxArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] > maxNumber) {
+          maxNumber = arr[i][j];
+        } else {
+          maxNumber = Math.max(...arr[i])
+        }
+      }
+      maxArray.push(maxNumber);
+      maxNumber = 0;
+    }
+    return maxArray;
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+  
